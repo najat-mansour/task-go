ee06fa08d0748d3f46f052caeacd023d47998845
najat-mansour <s12028099@stu.najah.edu>
2025-05-08 12:21:58 +0300

    initial commit

---
+++ src/test/java/com/taskgo/TaskGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
