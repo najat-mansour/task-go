19ed5c53b192b14a1106d03a33c02980db4d8270
najat-mansour <s12028099@stu.najah.edu>
2025-06-01 11:42:52 +0300

    updaing repositories

--- src/main/java/com/taskgo/repositories/WorkspaceRepository.java
+++ src/main/java/com/taskgo/repositories/WorkspaceRepository.java
@@ -4,6 +4,11 @@ import com.taskgo.entities.Workspace;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WorkspaceRepository extends JpaRepository<Workspace, String> {
+    List<Workspace> findAllByOwnerId(String ownerId);
+
+    Integer countAllByOwnerId(String ownerId);
 }
--- src/main/java/com/taskgo/repositories/WorkspaceViewerRepository.java
+++ src/main/java/com/taskgo/repositories/WorkspaceViewerRepository.java
@@ -4,9 +4,14 @@ import com.taskgo.entities.WorkspaceViewer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface WorkspaceViewerRepository extends JpaRepository<WorkspaceViewer, String> {
     Optional<WorkspaceViewer> findByWorkspaceIdAndViewerId(String workspaceId, String viewerId);
+
+    List<WorkspaceViewer> findAllByViewerId(String viewerId);
+
+    Integer countAllByViewerId(String viewerId);
 }
