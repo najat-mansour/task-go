20cefc550d7805c493972035887ad1c393b4d388
najat-mansour <s12028099@stu.najah.edu>
2025-06-01 11:49:49 +0300

    updating config: update WebSecurityConfig, add SwaggerConfig

--- src/main/java/com/taskgo/config/GlobalApiPrefixConfig.java
+++ src/main/java/com/taskgo/config/GlobalApiPrefixConfig.java
@@ -2,6 +2,7 @@ package com.taskgo.config;
 
 import com.taskgo.constants.URLs;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.pattern.PathPatternParser;
@@ -12,6 +13,10 @@ public class GlobalApiPrefixConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.setPatternParser(new PathPatternParser());
-        configurer.addPathPrefix(URLs.GLOBAL_API_PREFIX, clazz -> clazz.isAnnotationPresent(org.springframework.web.bind.annotation.RestController.class));
+        configurer.addPathPrefix(
+                URLs.GLOBAL_API_PREFIX,
+                clazz -> clazz.isAnnotationPresent(RestController.class)
+                        && !clazz.getPackageName().startsWith("org.springdoc")  // Exclude springdoc [OpenAPI  and Swagger]
+        );
     }
 }
---
+++ src/main/java/com/taskgo/config/WebSecurityConfig.java
@@ -0,0 +1,74 @@
+package com.taskgo.config;
+
+import com.taskgo.constants.URLs;
+import com.taskgo.security.AuthEntryPointJwt;
+import com.taskgo.security.AuthTokenFilter;
+import com.taskgo.security.JwtUtil;
+import com.taskgo.services.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService userDetailsService;
+    private final AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter(jwtUtil, userDetailsService);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        final String PERMITTED_AUTHENTICATION_URLs = URLs.GLOBAL_API_PREFIX + URLs.AUTHENTICATION_PREFIX + "/**";
+        final String PERMITTED_SIGN_UP_URL = URLs.GLOBAL_API_PREFIX + URLs.USERS_PREFIX + URLs.USERS_CREATE;
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(unauthorizedHandler)
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(
+                                        PERMITTED_AUTHENTICATION_URLs,
+                                        PERMITTED_SIGN_UP_URL,
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui/**"
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                );
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
